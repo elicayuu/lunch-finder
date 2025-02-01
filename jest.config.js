a064a3fe7369751297d4c2327cbe83d6753efbf5
Elica <ticci.zen@gmail.com>
2025-02-01 11:44:02 +0800

    feat(deps): add jest testing environment

---
+++ jest.config.js
@@ -0,0 +1,33 @@
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'jest-environment-jsdom',
+  transform: {
+    '^.+.tsx?$': [
+      'ts-jest',
+      {
+        diagnostics: false,
+        astTransformers: {
+          before: [
+            {
+              path: 'node_modules/ts-jest-mock-import-meta',
+              options: {
+                metaObjectReplacement: {
+                  env: {
+                    // Replicate as .env.local
+                    VITE_FSQ_API: 'Foursqure API TOKEN',
+                  },
+                },
+              },
+            },
+          ],
+        },
+      },
+    ],
+  },
+  moduleNameMapper: {
+    '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
+    '^.+\\.svg$': 'jest-transformer-svg',
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+}
---
+++ jest.setup.ts
@@ -0,0 +1,2 @@
+import '@testing-library/jest-dom'
+import 'core-js/stable/structured-clone'
