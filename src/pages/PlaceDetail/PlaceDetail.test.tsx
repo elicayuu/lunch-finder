9527fcebee87d21ef093eacbec3f5fe5023eb848
Elica <ticci.zen@gmail.com>
2025-02-02 18:35:39 +0800

    test(components): add more tests for PlaceDetail page

--- src/pages/PlaceDetail/PlaceDetail.test.tsx
+++ src/pages/PlaceDetail/PlaceDetail.test.tsx
@@ -17,13 +17,13 @@ const PlaceDetailWithWrapper: FC<{ placeId: string }> = ({ placeId }) => (
 )
 
 describe('<PlaceDetail />', () => {
-  const mockPlaceDetail = genPlaceDetail()
-
   beforeAll(() => server.listen())
   afterEach(() => server.resetHandlers())
   afterAll(() => server.close())
 
   test('show the restaurant detail info', async () => {
+    const mockPlaceDetail = genPlaceDetail()
+
     server.use(
       http.get(`${FSQ_API_URL}/${mockPlaceDetail.fsq_id}`, () =>
         HttpResponse.json(mockPlaceDetail),
@@ -43,7 +43,53 @@ describe('<PlaceDetail />', () => {
     )
   })
 
+  test('show message if no reviews', async () => {
+    const mockPlaceDetail = genPlaceDetail({ tips: [] })
+
+    server.use(
+      http.get(`${FSQ_API_URL}/${mockPlaceDetail.fsq_id}`, () =>
+        HttpResponse.json(mockPlaceDetail),
+      ),
+    )
+
+    render(<PlaceDetailWithWrapper placeId={mockPlaceDetail.fsq_id} />)
+
+    expect(await screen.findByText(/No reviews yet/i)).toBeInTheDocument()
+  })
+
+  test('should hide website info if no website data', async () => {
+    const mockPlaceDetail = genPlaceDetail({ website: '' })
+
+    server.use(
+      http.get(`${FSQ_API_URL}/${mockPlaceDetail.fsq_id}`, () =>
+        HttpResponse.json(mockPlaceDetail),
+      ),
+    )
+
+    render(<PlaceDetailWithWrapper placeId={mockPlaceDetail.fsq_id} />)
+
+    expect(await screen.findByText(mockPlaceDetail.name)).toBeInTheDocument()
+    expect(await screen.findByText(/website:/i)).not.toBeInTheDocument()
+  })
+
+  test('should hide menu info if no menu data', async () => {
+    const mockPlaceDetail = genPlaceDetail({ menu: '' })
+
+    server.use(
+      http.get(`${FSQ_API_URL}/${mockPlaceDetail.fsq_id}`, () =>
+        HttpResponse.json(mockPlaceDetail),
+      ),
+    )
+
+    render(<PlaceDetailWithWrapper placeId={mockPlaceDetail.fsq_id} />)
+
+    expect(await screen.findByText(mockPlaceDetail.name)).toBeInTheDocument()
+    expect(await screen.findByText(/menu:/i)).not.toBeInTheDocument()
+  })
+
   test('show error if the request fails', async () => {
+    const mockPlaceDetail = genPlaceDetail()
+
     server.use(
       http.get(`${FSQ_API_URL}/${mockPlaceDetail.fsq_id}`, () =>
         HttpResponse.error(),
